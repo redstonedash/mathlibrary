6987a5be4682f0c734699c2c2d1126f57aee72a6
Bread <redatonedash@gmail.com>
2019-01-18 10:45:47 -0800

    finsihed demo

--- math/UTILS.cpp
+++ math/UTILS.cpp
@@ -539,6 +539,13 @@ namespace math
 					0     ,      0,1);
 	}
 
+	mat3 mat3::scale(vec2 vec)
+	{
+		return mat3(vec.x, 0, 0,
+			0, vec.y, 0,
+			0, 0, 1);
+	}
+
 	void mat3::set(float m1, float m2, float m3, float m4, float m5, float m6, float m7, float m8, float m9)
 	{
 		this->m1 = m1;
--- raygame-master/raygame/main.cpp
+++ raygame-master/raygame/main.cpp
@@ -19,114 +19,72 @@
 
 int playerBeingGrabed = 1; //player 1, no one, player 2
 
-struct Player {
+struct Node {
 	int playerNumber;
 	math::mat3 transform;
-	math::mat3 translation;
-	float localRotation; //TODO: rework rotation
-	math::mat3 scale;
-	math::mat3 grabOffset;
+	math::vec2 translation;
+	float rotation; //TODO: rework rotation
+	math::vec2 scale;
+	Node* child;
+	Color c;
 	/*
 	[Xx][Yx][Tx]
 	[Xy][Yy][Ty]
 	[ 0][ 0][ 1]
 	*/
-	math::vec2 velocity = { 0, 0};
-	math::vec2 gamePadLastPos = { 0, 0 };
-	float hitstun = 0;
-	float stun = 0;
-	//float invincibility; //if i have time
-	float damage = 0;
-	float lag = 0;
-	Player(){}
-	Player(int playerNumber) {
-		this->playerNumber = playerNumber;
-	}
 };
 
 
-Player playerArr[2];
-void Draw(Player p) {
-	Rectangle rect = Rectangle();
-	rect.x = p.transform.m3;
-	rect.y = p.transform.m6;
-	rect.width  = 100;
-	rect.height = 100;
-	DrawRectanglePro(rect, { 0,  0}, math::vec2(p.rotation.m1, p.rotation.m4).angleBetween({ 0,1 })*math::RAD_TO_DEG , 
-		{ (unsigned char)(155 + 155 + (100 * p.playerNumber)),(unsigned char)(100 * p.playerNumber),(unsigned char)(155 + (100 * p.playerNumber)),(unsigned char)255 });
-	//DrawRectanglePro(rect, { 500,500 }, i, PURPLE);
+void Draw(Node n, Node* Offset) {
+	float scaleOffset = 50; // makes lines long enough to be visable without effecting scale
+	n.transform = math::mat3::translation(n.translation) * math::mat3::rotation(n.rotation) * math::mat3::scale(n.scale);
+	if (!Offset) {
+		Offset = &n;
+	} else {
+		Offset->transform = Offset->transform * n.transform;
+	}
+	DrawLine(Offset->transform.mm[0][2], Offset->transform.mm[1][2], Offset->transform.mm[0][2] + Offset->transform.mm[0][0]*scaleOffset, Offset->transform.mm[1][2] + Offset->transform.mm[1][0]*scaleOffset, RED);
+	DrawLine(Offset->transform.mm[0][2], Offset->transform.mm[1][2], Offset->transform.mm[0][2] + Offset->transform.mm[0][1]*scaleOffset, Offset->transform.mm[1][2] + Offset->transform.mm[1][1]*scaleOffset, BLUE);
+	if (n.child) {
+		Draw(*n.child, Offset);
+	}
 }
 
-void Update(Player *p) {
-	p->translation.m3 += p->velocity.x;
-	p->translation.m6 += p->velocity.y;
-	p->velocity.y += GetFrameTime()*10;
-	/*PLAYER INPUT*/
-	/*BEING GRABED*/
-	/*DISABLED*/
-	bool disabled = false;
-	if (p->lag>0) {
-		p->lag -= GetFrameTime();
-		disabled = true;
-	}
-	if (true) {
-		p->stun -= GetFrameTime();
-		std::cout << GetFrameTime() << "\n";
-		p->rotation *= math::mat3::rotation(GetFrameTime()*30); //TODO: delete this
-		disabled = true;
-	}
-	if (p->hitstun > 0) {
-		p->hitstun -= GetFrameTime();
-		disabled = true;
-	}
-	if (disabled) goto applyTransformBeforeFunctionExit; //forgive me for i have sinned
-	if (IsGamepadAvailable(p->playerNumber))
-	{
-		
-		/*DODGE/GRAB*/
-		if ((math::vec2(GetGamepadAxisMovement(p->playerNumber, 0),
-			GetGamepadAxisMovement(p->playerNumber, 1)) - p->gamePadLastPos).magnitude() > 0.9 && math::vec2(GetGamepadAxisMovement(p->playerNumber, 0),
-				GetGamepadAxisMovement(p->playerNumber, 1)).magnitude()>0.5) {
-			//p->lag
-		}
-		/*THROW*/
-
-		/*MOVEMENT*/
-		p->velocity.x += GetGamepadAxisMovement(p->playerNumber, 0)*GetFrameTime()*30;
-		if (GetGamepadAxisMovement(p->playerNumber, 1)*GetFrameTime() > 0 && p->velocity.y) {
-			p->velocity.y += GetGamepadAxisMovement(p->playerNumber, 1) * GetFrameTime() * 40;
-		}
-		p->gamePadLastPos = {
-			GetGamepadAxisMovement(p->playerNumber, 0),
-			GetGamepadAxisMovement(p->playerNumber, 1)
-		};
-	}
-	applyTransformBeforeFunctionExit: //NEVER CALL RETURN IN THIS FUNCTION! ALWAYS GO HERE INSTEAD!
-	p->transform = p->scale*p->translation*p->rotation;
-	return;
-}
 
 int main()
 {
-	playerArr[0] = Player(GAMEPAD_PLAYER1);
-	playerArr[1] = Player(GAMEPAD_PLAYER2);
-	playerArr[0].transform = math::mat3(0, 0, 0, 
-										0, 0, 0,
-										0, 0, 1);
-	playerArr[1].transform = math::mat3(0, 0, 0,
-										0, 0, 0,
-										0, 0, 1);
-	playerArr[0].translation = math::mat3::translation(200, 200);
-	playerArr[1].translation = math::mat3::translation(800, 100);
-	playerArr[0].rotation = math::mat3::identity();
-	playerArr[1].rotation = math::mat3::identity();
-	playerArr[0].scale = math::mat3::scale(1,1);
-	playerArr[1].scale = math::mat3::scale(-1,1);
-
 	// Initialization
 	//--------------------------------------------------------------------------------------
 	int screenWidth = 1060;
 	int screenHeight = 640;
+
+	Node node = Node();
+	node.transform = math::mat3(1, 0, 0,
+								0, 1, 0,
+								0, 0, 1);
+	node.translation = math::vec2(screenWidth/2, screenHeight/2);
+	node.rotation = 0;
+	node.scale = math::vec2(1,1);
+	node.c = BLACK;
+	Node child = Node();
+	child.transform = math::mat3(1, 0, 0,
+	      0, 1, 0,
+	      0, 0, 1);
+	child.translation = math::vec2(50, 100);
+	child.rotation = 0;
+	child.scale = math::vec2(1, 1);
+	child.c = BLACK;
+	node.child = &child;
+
+	Node spinner = Node();
+	spinner.transform = math::mat3(1, 0, 0,
+		0, 1, 0,
+		0, 0, 1);
+	spinner.translation = math::vec2(50, 100);
+	spinner.rotation = 0;
+	spinner.scale = math::vec2(1, 1);
+	spinner.c = BLACK;
+	child.child = &spinner;
 
 	SetConfigFlags(FLAG_VSYNC_HINT);
 
@@ -156,20 +114,86 @@ int main()
 		BeginDrawing();
 
 		ClearBackground(WHITE);
-		if (OddFrame) {
-			Update(&playerArr[0]);
-			Update(&playerArr[1]);
-		} else {
-			Update(&playerArr[1]);
-			Update(&playerArr[0]);
+		//node.rotation += GetFrameTime();
+		if (GetKeyPressed() >= 0) {
+			std::cout << GetKeyPressed() << "\n";
+			switch (GetKeyPressed())
+			{
+			case 113:
+				node.rotation += math::Tau / 16;
+				break;
+			case 119:
+				node.rotation -= math::Tau / 16;
+				break;
+			case 101:
+				node.scale.x += 1;
+				break;
+			case 114:
+				node.scale.x -= 1;
+				break;
+			case 116:
+				node.scale.y += 1;
+				break;
+			case 121:
+				node.scale.y -= 1;
+				break;
+			case 117:
+				node.translation.x += 5;
+				break;
+			case 105:
+				node.translation.x -= 5;
+				break;
+			case 111:
+				node.translation.y += 5;
+				break;
+			case 112:
+				node.translation.y -= 5;
+				break;
+			default:
+				break;
+			}
+			switch (GetKeyPressed())
+			{
+			case 97:
+				child.rotation += math::Tau / 16;
+				break;
+			case 115:
+				child.rotation -= math::Tau / 16;
+				break;
+			case 100:
+				child.scale.x += 1;
+				break;
+			case 102:
+				child.scale.x -= 1;
+				break;
+			case 103:
+				child.scale.y += 1;
+				break;
+			case 104:
+				child.scale.y -= 1;
+				break;
+			case 106:
+				child.translation.x += 5;
+				break;
+			case 107:
+				child.translation.x -= 5;
+				break;
+			case 108:
+				child.translation.y += 5;
+				break;
+			case 59:
+				child.translation.y -= 5;
+				break;
+			default:
+				break;
+			}
 		}
-		Draw(playerArr[0]);
-		Draw(playerArr[1]);
+		spinner.rotation += GetFrameTime();
+		Draw(node, nullptr);
 		DrawFPS(0, 0);
 
 		EndDrawing();
 		//----------------------------------------------------------------------------------
-		OddFrame = !OddFrame;
 	}
 
 	// De-Initialization
